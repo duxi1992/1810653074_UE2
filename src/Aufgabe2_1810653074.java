2881252b6427622a4944b18e75f342725be7e972
Riccardo Duxenneuner <riccardo@duxi.at>
2018-11-26 17:44:54 +0100

    UE2_aufgabe2

---
+++ src/Aufgabe2_1810653074.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class Aufgabe2_1810653074 {
+    public static void main(String[] args) {
+
+        // eingabefeld fom Typ int für 2 Variablen
+        int zahl1 = Integer.parseInt(JOptionPane.showInputDialog("bitte geben sie eine Zahl ein!"));
+        int zahl2 = Integer.parseInt(JOptionPane.showInputDialog("bitte geben sie noch eine Zahl ein!"));
+
+        if (zahl1 == zahl2) {
+
+            System.out.println("Deine zahlen stimmen überein!");
+
+            // Prfüt ob der Wert kleiner oder größer als der andere Wert ist
+        } else if (zahl1 < zahl2) {
+            for (int i = zahl1; i <= zahl2; i++) {
+                //Ausgabe der benötigten Schritte
+                System.out.println("esbenötigte " + i + " Schritte");
+            }
+        } else {
+            for (int i = zahl2; i <= zahl1; i++) {
+                //Ausgabe der benötigten Schritte
+                System.out.println("Es benötigte " + i + " Schritte");
+            }
+        }
+    }
+}
